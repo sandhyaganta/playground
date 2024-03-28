bc6d865af895c5ccb69b92d44fa62ebd88768c1f
sandhyaganta <gantasandhyavitasoft@gmail.com>
2024-03-28 15:26:02 +0530

    stadium component completed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { GamesSportsComponent } from './games-sports/games-sports.component';
 import { CricketComponent } from './cricket/cricket.component';
 import { BatmintonComponent } from './batminton/batminton.component';
+import { StadiumComponent } from './stadium/stadium.component';
 
 export const routes: Routes = [
     {path:'',component:HomePageComponent},
     {path:'homepage',component:HomePageComponent},
     {path:'games',component:GamesSportsComponent},
     {path:'cricket',component:CricketComponent},
-    {path:'batminton',component:BatmintonComponent}
+    {path:'batminton',component:BatmintonComponent},
+    {path:'stadium',component:StadiumComponent}
     
 ];
