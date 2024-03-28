d76d4ff75052339e99897e2270a5ad821c7075d3
sandhyaganta <gantasandhyavitasoft@gmail.com>
2024-03-28 12:44:59 +0530

    cricket componet completed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { GamesSportsComponent } from './games-sports/games-sports.component';
+import { CricketComponent } from './cricket/cricket.component';
 
 export const routes: Routes = [
     {path:'',component:HomePageComponent},
     {path:'homepage',component:HomePageComponent},
-    {path:'games',component:GamesSportsComponent}
+    {path:'games',component:GamesSportsComponent},
+    {path:'cricket',component:CricketComponent}
     
 ];
