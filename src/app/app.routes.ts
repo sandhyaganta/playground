d107e4a8ed570771cba5e686974efaf21ba0df6d
sandhyaganta <gantasandhyavitasoft@gmail.com>
2024-03-28 10:16:00 +0530

    home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',component:HomePageComponent},
+    {path:'homepage',component:HomePageComponent}
+    
+];
